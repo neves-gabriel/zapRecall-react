55bb279fd0cf79193c8f8355000da6852beac5ad
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-12-13 12:55:00 -0300

    add: new decks to config file

--- src/components/StartPracticeBtn.js
+++ src/components/StartPracticeBtn.js
@@ -4,7 +4,7 @@ export default function StartPracticeBtn(props) {
 
     return (
         <button className="start-practice-btn" onClick={() => props.selectDeck(props.index)}>
-            <span className='deck-name'>{props.deckName}</span>
+            <span className='deck-name'>Praticar {props.deckName}</span>
             <span className='forward-icon'/>
             <span className='forward-icon'/>
         </button>
--- src/config/config.js
+++ src/config/config.js
@@ -1,35 +1,75 @@
 const decksData = [
     {
-        name: "Praticar JavaScript",
+        name: "JavaScript",
+        cards: [
+            {
+                question: "Como é inserido JavaScript em uma pagina web?",
+                answer: "com a script tag:<script>"
+            },
+            {
+                question: "Aonde colocamos os scripts?",
+                answer: "Nas seções Body e Head"
+            },
+            {
+                question: "Quando o JavaScript é executado?",
+                answer: "Imediatamente, assim que a página carregar"
+            },
+            {
+                question: "Quantos scripts uma página pode conter?",
+                answer: "Quantidade ilimitada"
+            },
+            {
+                question: "Quantos scripts uma página pode conter?",
+                answer: "Quantidade ilimitada"
+            },
+            {
+                question: "De acordo com as boas práticas, onde as variaveis devem ser declaradas?",
+                answer: "Em um unico lugar no começo do código"
+            },
+            {
+                question: "Booleanos são usados em __?",
+                answer: "testes condicionais"
+            },
+            {
+                question: "Um bloco de código que é executado quando chamado é?",
+                answer: "Uma função"
+            },
+        ],
+    },
+    {
+        name: "React",
         cards: [
             {
                 question: "O que é JSX?",
                 answer: "Uma extensão de linguagem do JavaScript"
             },
             {
-                question: "Podemos colocar __ dentro do JSX?",
-                answer: "Expressões"
-            },
-            {
-                question: "O ReactDOM nos ajuda __",
-                answer: "Interagindo com a DOM para colocar componentes React na mesma"
-            },
-        ],
-    },
-    {
-        name: "Praticar React",
-        cards: [
-            {
                 question: "O React é __",
-                answer: "Uma biblioteca JavaScript para construção de interfaces"
+                answer: "uma biblioteca JavaScript para construção de interfaces"
             },
             {
                 question: "Componentes devem iniciar com __",
-                answer: "Letra maiúscula"
+                answer: "letra maiúscula"
+            },
+            {
+                question: "Podemos colocar __ dentro do JSX",
+                answer: "expressões"
+            },
+            {
+                question: "O ReactDOM nos ajuda __",
+                answer: "interagindo com a DOM para colocar componentes React na mesma"
             },
             {
                 question: "Usamos o npm para __",
-                answer: "Gerenciar os pacotes necessários e suas dependências"
+                answer: "gerenciar os pacotes necessários e suas dependências"
+            },
+            {
+                question: "Usamos props para __",
+                answer: "passar diferentes informações para componentes"
+            },
+            {
+                question: "Usamos estado (state) para __",
+                answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
             },
         ],
     }
