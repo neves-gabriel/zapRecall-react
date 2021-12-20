dbf06c5622993bd452a3b096428562c87c145e1e
Guga <contato.gustavobarbosa@outlook.com>
2021-12-20 11:23:41 -0300

    Preparando revisão do código

--- src/components/TurnCardBtn.js
+++
@@ -1,8 +0,0 @@
-import TURN_ICON from '../assets/img/turn-icon.svg'
-
-export default function TurnCardBtn(props) {
-
-    return (
-        <img className="turn-icon" src={TURN_ICON} alt="turn-icon" onClick={props.turnCard} data-identifier="arrow"/>
-    )
-}
